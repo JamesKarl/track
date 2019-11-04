9d94e51ada9cd2dfece3148d2f6a971d3d76e18a
柯军 <kej@meiyibao.com>
2019-11-04 17:31:59 +0800

    some changes.

--- app/src/main/java/com/example/track/EventReceiver.kt
+++ app/src/main/java/com/example/track/EventReceiver.kt
@@ -3,9 +3,14 @@ package com.example.track
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 class EventReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
 
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.e("xxxxx", "some event: ${intent?.action}")
+        LocationUtils.startLocate {
+            Log.d("xxxx", "address: $it")
+        }
     }
 }
