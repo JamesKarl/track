afeee3cf416fdabc76fd3af7104e9cb8f433fb12
柯军 <kej@meiyibao.com>
2019-11-04 16:21:31 +0800

    权限修改

--- app/src/main/java/com/example/track/LocationUtils.kt
+++ app/src/main/java/com/example/track/LocationUtils.kt
@@ -96,9 +96,9 @@ object LocationUtils : AMapLocationListener {
         val mOption = AMapLocationClientOption()
         mOption.locationMode =
             AMapLocationClientOption.AMapLocationMode.Hight_Accuracy//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
-        mOption.isGpsFirst = false//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
+        mOption.isGpsFirst = true//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
         mOption.httpTimeOut = 30000//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
-        mOption.interval = 2000//可选，设置定位间隔。默认为2秒
+        mOption.interval = if (BuildConfig.DEBUG == true) 2000 else 10000//可选，设置定位间隔。默认为2秒
         mOption.isNeedAddress = true//可选，设置是否返回逆地理地址信息。默认是true
         mOption.isOnceLocation = false//可选，设置是否单次定位。默认是false
         mOption.isOnceLocationLatest = false//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
